5981f656bf455ce6fcd45695176fffeafbcfad15
Timileyin Israel Afolayan <139009300+justtimi@users.noreply.github.com>
2026-07-13 21:01:24 +0100

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_set = {}
+        t_set = {}
+
+        for i in range(len(s)):
+            s_set[s[i]] = s_set.get(s[i], 0) + 1
+            t_set[t[i]] = t_set.get(t[i], 0) + 1
+
+        return s_set == t_set
+
+        
